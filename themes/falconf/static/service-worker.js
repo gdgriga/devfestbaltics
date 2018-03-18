737c279980434ca6b8f0faf2e14b995730052ed9
Sergey Kibish <sergey.kibish@gmail.com>
2018-03-18 19:22:39 +0200

    Add script to remove caches

--- themes/falconf/static/service-worker.js
+++ themes/falconf/static/service-worker.js
@@ -1,8 +1,17 @@
 "use strict";
 
-// hack to unregister old service workers
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-  for(let registration of registrations) {
-    registration.unregister();
-  }
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
